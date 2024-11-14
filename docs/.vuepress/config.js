8758cad93719e7cb5f83cbbf474d645ab27c14bb
刘世豪 <168500981@qq.com>
2024-11-14 21:28:59 +0800

    测试actions

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,10 +5,19 @@ import { plumeTheme } from 'vuepress-theme-plume'
 export default defineUserConfig({
   // 请不要忘记设置默认语言
   lang: 'zh-CN',
-  port: '8099',
+  port: '80',
+  title: 'Uiiiii  （一个u五个i）',
+  description: '这是我的第一个 VuePress 站点',
   theme: plumeTheme({
     
     // more...
+    footer: {
+      message: `
+        <a href="https://beian.miit.gov.cn/" target="_blank">豫ICP备2024093349号</a> | Copyright © 2016-2024
+      `,
+      // copyright: '111111111',
+    }
   }),
   bundler: viteBundler(),
+
 })
---
+++ docs/index.md
@@ -0,0 +1,26 @@
+---
+pageLayout: home
+
+config:
+- type: banner
+  banner: https://api.pengzhanbo.cn/wallpaper/bing
+  bannerMask:
+  light: 0.1
+  dark: 0.3
+  hero:
+    name: 小酥饼R
+    tagline: Front End Developer
+    text: 即使慢，驰而不息，纵会落后，纵会失败，但必须能够到达他所向的目标。
+    actions:
+      -
+        text: 我的博客
+        link: /blog/
+        theme: brand
+      - 
+        text: Github
+        link: https://github.com/pengzhanbo
+        theme: alt
+
+- type: custom
+footer: nihao
+---
